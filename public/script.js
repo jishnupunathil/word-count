dce9a33d69db01db28f790218c5eacb4c213eb28
Jishnu <jishnupunathil000@gmail.com>
2023-06-10 02:34:03 +0530

    remove added

--- public/script.js
+++ public/script.js
@@ -12,28 +12,51 @@ function getWordCount(url, callback) {
 
 // Function to send the word count to the server
 function saveWordCount(url, wordCount) {
-    const data = { url, wordCount };
-  
-    fetch('/api/wordcount', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+  const data = { url, wordCount };
+
+  fetch('/api/wordcount', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Word count saved successfully');
+        // Display the updated word count history
+        getWordCountHistory();
+      } else {
+        console.error('Error saving word count');
+      }
     })
-      .then(response => {
-        if (response.ok) {
-          console.log('Word count saved successfully');
-          // Display the updated word count history
-          getWordCountHistory();
-        } else {
-          console.error('Error saving word count');
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// Function to remove word count entry
+function removeWordCount(url, count) {
+  fetch('/api/wordcount', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url, count })
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Word count removed successfully');
+        // Refresh the word count history
+        getWordCountHistory();
+      } else {
+        console.error('Error removing word count');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
 
 // Function to display the word count history on the webpage
 function displayWordCountHistory(wordCounts) {
@@ -78,7 +101,6 @@ function handleButtonClick() {
   getWordCount(url, wordCount => {
     saveWordCount(url, wordCount);
 
-
     // Display the word count in the <h2> tag
     const wordCountElement = document.getElementById('wordCount');
     wordCountElement.textContent = wordCount;
@@ -91,6 +113,17 @@ function initialize() {
   button.addEventListener('click', handleButtonClick);
 
   getWordCountHistory();
+
+  // Event listener for click events on the table
+  document.getElementById('historyTable').addEventListener('click', function (event) {
+    const clickedElement = event.target;
+    if (clickedElement.classList.contains('removeButton')) {
+      const tableRow = clickedElement.closest('tr');
+      const url = tableRow.cells[0].textContent;
+      const count = tableRow.cells[1].textContent;
+      removeWordCount(url, count);
+    }
+  });
 }
 
 // Call the initialize function when the webpage loads
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const axios=require('axios')
-const cheerio=require('cheerio')
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Temporary storage for word counts and domains
 let wordCounts = [];
@@ -17,39 +17,48 @@ let domains = [];
 
 // Retrieve Word Count
 app.get('/api/wordcount', (req, res) => {
-    const { url } = req.query;
-    
-    // Logic to fetch website content and calculate word count
-    axios.get(url)
-      .then(response => {
-        const htmlContent = response.data;
-        const $ = cheerio.load(htmlContent);
-        const text = $('body').text();
-        const wordCount = text.split(' ').length;
-        
-        // Return the word count as a JSON response
-        res.json({ wordCount });
-      })
-      .catch(error => {
-        res.status(500).json({ error: 'Error retrieving word count' });
-      });
-  })
+  const { url } = req.query;
 
-  app.get('/api/history', (req, res) => {
-    res.json(wordCounts);
-  });
+  // Logic to fetch website content and calculate word count
+  axios.get(url)
+    .then(response => {
+      const htmlContent = response.data;
+      const $ = cheerio.load(htmlContent);
+      const text = $('body').text();
+      const wordCount = text.split(' ').length;
 
-  app.post('/api/wordcount', (req, res) => {
-    const { url, wordCount } = req.body;
-    
-    // Save the word count to the server
-    const wordCountEntry = { url, count: wordCount };
-    wordCounts.push(wordCountEntry);
-    
-    // Send a success response
-    res.status(201).json({ message: 'Word count saved successfully' });
-  });
+      // Return the word count as a JSON response
+      res.json({ wordCount });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error retrieving word count' });
+    });
+});
 
+app.get('/api/history', (req, res) => {
+  res.json(wordCounts);
+});
+
+app.post('/api/wordcount', (req, res) => {
+  const { url, wordCount } = req.body;
+
+  // Save the word count to the server
+  const wordCountEntry = { url, count: wordCount };
+  wordCounts.push(wordCountEntry);
+
+  // Send a success response
+  res.status(201).json({ message: 'Word count saved successfully' });
+});
+
+app.delete('/api/wordcount', (req, res) => {
+  const { url, count } = req.body;
+
+  // Remove the word count entry from the server
+  wordCounts = wordCounts.filter(entry => entry.url !== url || entry.count !== parseInt(count));
+
+  // Send a success response
+  res.status(200).json({ message: 'Word count removed successfully' });
+});
 
 const port = 3000;
 app.listen(port, () => {
